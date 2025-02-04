cbf9ec5b549c1be5d7e5cdf638b25a71b0d16c9a
kameera135 <kameera135@users.noreply.github.com>
2025-02-04 12:05:06 +0530

    Database Migration

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,5 +2,10 @@
 {
     public class Product
     {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
     }
 }
--- obj/Debug/net8.0/ProductManagementApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProductManagementApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5bc1da7c9bcaa27f42b838deab60f08a0df0a07")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
